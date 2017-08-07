bd015ce7db1c54991870d6ccd6c1b319b5706f5a
Chris <cjones999983@gmail.com>
2017-08-07 13:33:56 -0400

    Working CLI, working on updating Computer player

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,31 @@
+require "pry"
+
 module Players
   class Computer < Player
+    attr_accessor :second_move
+
+    WIN_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [0, 4, 8]
+    ]
+
+    SECOND_MOVE = ["1", "3", "7", "9"]
 
     def move(board)
+      if board.turn_count == 0
+        "5"
+      elsif board.turn_count == 1
+        binding.pry
+        SECOND_MOVE.sample
+      else
       (1..9).to_a.sample.to_s
+      end
     end
     
   end
